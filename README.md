e403728d639b53f6360849efa2378aab910cebb4
sfell77 <github@sfproductions.net>
2021-06-25 14:38:14 -0400

    wrapping up prep work

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # app_grafana
-Granfana deployment configuration
+Grafana deployment configuration leveraging a basic HA architecture (not application) strategy.
+
+If this was a completely HA strategy, the the minimum design would be:
+- multiple Grafana instances running in each region (use containers/Fargate and never have to manage EC2s)
+- RDS backend (replicated to another region) to ensure data retention and centralization amongst numerous running instances
+- Route53 entrypoint with failover to alternate region (active/passive) or if you are more geographically diverse, active/active with geolocation routing
---
+++ main.tf
@@ -0,0 +1,21 @@
+provider = "aws" {
+    version = "~> 3.37"
+}
+
+module "arch_ec2_asg_elb" {
+    source = "git::https://github.com/sfell77/arch_ec2_asg_elb"
+    app_name                = var.app_name
+    aws_region              = var.aws_region
+    ami_id                  = var.ami_id
+    deploy_env              = var.deploy_env
+    asg_max_size            = var.asg_max_size
+    asg_min_size            = var.asg_min_size
+    asg_desired_size        = var.asg_desired_size
+    instance_type           = var.instance_type
+    volume_type             = var.ebs_volume_type
+    volume_size             = var.ebs_volume_size
+    s3_bucket_name          = var.s3_bucket_name
+    tag_owner_contact       = var.tag_owner_contact
+    tag_deployment_owner    = var.tag_deployment_owner
+    #security_groups         = var.security_groups
+}
