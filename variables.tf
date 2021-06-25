8ee490375bfa6c6e5485b8b1f0737282e1fd4330
sfell77 <github@sfproductions.net>
2021-06-25 14:57:51 -0400

    variables done'ish

--- variables.tf
+++ variables.tf
@@ -13,6 +13,40 @@ var "tag_owner_contact" {
     default = "sfellin@sfproductions.net"
 }
 
+var "asg_max_size" {
+    default = 1
+}
+
+var "asg_min_size" {
+    default = 1
+}
+
+var "asg_desired_size" {
+    default = 1
+}
+
+var "instance_type" {
+    default = ""
+}
+
+var "ebs_volume_type" {
+    default = "gp2"
+}
+
+var "ebs_volume_size" {
+    default = 20
+}
+
+var "s3_bucket_name" {
+    description = "We save statefiles to S3 so that they're in a centralized location that's not your laptop. Remember to create buckets PER REGION or you may not have access to your statefiles in the event of a regional outage"
+    default = {
+        "dev.us-east-1"     = ""
+        "qa.us-east-1"      = ""
+        "dev.us-west-2"     = ""
+        "qa.us-west-2"      = ""
+    }
+
+
 # Variables you'll pass at run time -- you can leave these blank unless they're region agnostic
 var "ami_id" {
     description = "The AMI ID you'll use for your EC2 (if you're using one).  We don't use 'latest' since that doesn't stay the same, and we potentially haven't validated that it works with our stack"
@@ -24,10 +58,6 @@ var "aws_region" {
 
 var "deploy_env" {
     description = "Denotes dev/qa/prod regions (or whatever values you use); needed for map variables"
-}
-
-var "s3_bucket_name" {
-    description = "We save statefiles to S3 so that they're in a centralized location that's not your laptop. Remember to create buckets PER REGION or you may not have access to your statefiles in the event of a regional outage"
 }
 
 var "tag_deployment_owner" {
