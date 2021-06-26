8d0f15e6e876172e78c4753bfd8c3f0f4b44acf7
sfell77 <github@sfproductions.net>
2021-06-26 16:37:58 -0400

    rename var

--- main.tf
+++ main.tf
@@ -7,20 +7,15 @@ terraform {
 module "arch_ec2_asg_elb" {
     source                  = "git::https://github.com/sfell77/arch_ec2_asg_elb"
     ami_id                  = lookup(var.ami_id, format("%s.%s", var.app_env, var.aws_region))
+    app_env                 = var.app_env
     app_name                = var.app_name
     app_port                = var.app_port
-    asg_desired_size        = var.asg_desired_size
-    asg_max_size            = var.asg_max_size
-    asg_min_size            = var.asg_min_size
     aws_region              = var.aws_region
-    deploy_env              = var.deploy_env
-    instance_type           = var.instance_type
+    hc_target               = var.hc_target
     s3_bucket_name          = lookup(var.s3_bucket_name, format("%s.%s", var.app_env, var.aws_region))
     subnets                 = lookup(var.app_subnets, format("%s.%s", var.app_env, var.aws_region))
     tag_deployment_owner    = var.tag_deployment_owner
     tag_owner_contact       = var.tag_owner_contact
-    volume_size             = var.ebs_volume_size
-    volume_type             = var.ebs_volume_type
     user_data               = "${file("config/user_data.sh")}"
 }
 
--- variables.tf
+++ variables.tf
@@ -11,15 +11,20 @@ variable "app_port" {
 variable "app_subnets" {
     description = "Subnet(s) for the ELB to leverage"
     default = {
-        "dev.us-east-1"     = ""
+        "dev.us-east-1"     = "subnet-0ea9e47983f203578, subnet-021f28cc2574fc87e"
     }
 }
 
 variable "ami_id" {
     description = "The AMI ID you'll use for your EC2 (if you're using one).  We don't use 'latest' since that doesn't stay the same, and we potentially haven't validated that it works with our stack"
     default = {
-        "dev.us-east-1"     = ""
+        "dev.us-east-1"     = "ami-0aeeebd8d2ab47354"
     }
+}
+
+variable "hc_target" {
+    description = "ELB health check -- target"
+    default = "HTTP:3000/"
 }
 
 variable "listeners" {
@@ -37,7 +42,7 @@ variable "listeners" {
 variable "s3_bucket_name" {
     description = "We save statefiles to S3 so that they're in a centralized location that's not your laptop. Remember to create buckets PER REGION or you may not have access to your statefiles in the event of a regional outage"
     default = {
-        "dev.us-east-1"     = ""
+        "dev.us-east-1"     = "cribl-test-jlq818"
     }
 }
 
@@ -47,12 +52,12 @@ variable "tag_owner_contact" {
 }
 
 # Variables you'll pass at run time -- you can leave these blank unless they're region agnostic
-variable "aws_region" {
-    description = "The AWS region you're deploying to.  For example, us-east-1"
+variable "app_env" {
+    description = "Denotes dev/qa/prod regions (or whatever values you use); needed for map variables"
 }
 
-variable "deploy_env" {
-    description = "Denotes dev/qa/prod regions (or whatever values you use); needed for map variables"
+variable "aws_region" {
+    description = "The AWS region you're deploying to.  For example, us-east-1"
 }
 
 variable "tag_deployment_owner" {
