4826fb0955374cc08565aad1a8e88699cf49639b
sfell77 <github@sfproductions.net>
2021-06-26 16:57:21 -0400

    s

--- main.tf
+++ main.tf
@@ -13,10 +13,11 @@ module "arch_ec2_asg_elb" {
     aws_region              = var.aws_region
     deployment_owner        = var.deployment_owner
     hc_target               = var.hc_target
+    listeners               = var.listeners
     owner_contact           = var.owner_contact
     s3_bucket_name          = lookup(var.s3_bucket_name, format("%s.%s", var.app_env, var.aws_region))
     subnets                 = lookup(var.app_subnets, format("%s.%s", var.app_env, var.aws_region))
-    user_data               = "${file("config/user_data.sh")}"
+    user_data               = "${file("configs/user_data.sh")}"
 }
 
 # I tend to use variables for EVERYTHING instead of just the things that can't be static-defined (app_name, for example, will always be "Grafana" for Grafana) because this puts ALL the variables into one location (variables.tf) instead of having to bounce around between main and variables.
