66d388405305fb36c13ad969af1b4a78989d6a09
sfell77 <github@sfproductions.net>
2021-06-25 23:12:13 -0400

    lookup AMI, S3

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "arch_ec2_asg_elb" {
     source = "git::https://github.com/sfell77/arch_ec2_asg_elb"
     app_name                = var.app_name
     aws_region              = var.aws_region
-    ami_id                  = var.ami_id
+    ami_id                  = lookup(var.ami_id, format("%s.%s", var.app_env, var.aws_region))
     deploy_env              = var.deploy_env
     asg_max_size            = var.asg_max_size
     asg_min_size            = var.asg_min_size
@@ -14,7 +14,7 @@ module "arch_ec2_asg_elb" {
     instance_type           = var.instance_type
     volume_type             = var.ebs_volume_type
     volume_size             = var.ebs_volume_size
-    s3_bucket_name          = var.s3_bucket_name
+    s3_bucket_name          = lookup(var.s3_bucket_name, format("%s.%s", var.app_env, var.aws_region))
     tag_owner_contact       = var.tag_owner_contact
     tag_deployment_owner    = var.tag_deployment_owner
     #security_groups         = var.security_groups
